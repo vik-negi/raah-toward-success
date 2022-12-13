c651752ccc29980ce977da104087ac1883858d98
Ri-2020 <rohitgupta111abcd@gmail.com>
2022-12-13 10:18:26 +0530

    ash

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,9 +1,41 @@
 
 
-const changeActive = (tochange) =>{
-    const x =document.getElementsByClassName("active");
-    x[0].classList.toggle("active");
-    tochange.classList.tochange("active");
-}
+const profile = document.getElementById("profile");
+const submissions = document.getElementById("submissions");
+const resources = document.getElementById("resources");4
+
+profile.addEventListener("click" , ()=>{
+    console.log("called navigation button")
+    if(submissions.classList.contains("active")){
+        submissions.classList.toggle("active");
+        profile.classList.toggle("active")
+    }
+    if(resources.classList.contains("active")){
+        resources.classList.toggle("active");
+        profile.classList.toggle("active");
+    }
+})
+submissions.addEventListener("click" , ()=>{
+    console.log("called navigation button")
+    if(profile.classList.contains("active")){
+        profile.classList.toggle("active")
+        submissions.classList.toggle("active");
+    }
+    if(resources.classList.contains("active")){
+        resources.classList.toggle("active");
+        submissions.classList.toggle("active");
+    }
+})
+resources.addEventListener("click" , ()=>{
+    console.log("called navigation button")
+    if(profile.classList.contains("active")){
+        profile.classList.toggle("active")
+        resources.classList.toggle("active");
+    }
+    if(submissions.classList.contains("active")){
+        submissions.classList.toggle("active");
+        resources.classList.toggle("active");
+    }
+})
 
 
